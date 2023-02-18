90fbe6fa1baa550cd5d8febd13927a8c1a9f65b6
rintha <rintharithu@gmail.com>
2023-02-19 01:11:02 +0530

    first commit

---
+++ src/components/Todo.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import './Todo.css';
+
+function Todo() {
+  
+
+  return (
+    <div className="Todo">
+     todo
+    </div>
+  );
+}
+
+export default Todo;
